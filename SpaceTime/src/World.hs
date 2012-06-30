laithalissa/Serenity-prototypes

commit 3f2aef3cfcfbff94ba847ac15c79f08fee7b60b2

Continous evolution when holding left/right arrows.
The evolving flag now has three states. Evolving when moving forward in
time, devolving when moving backward and static otherwise.

SpaceTime/src/World.hs
@@ -16,7 +16,7 @@ data World = World
 	,	slice          :: Int
 	,	time           :: Float
 	,	c_history      :: [Slice]
-	,	evolving       :: Bool
+	,	evolving       :: Evolving
 	,	last_tick      :: Float
 	,	mode           :: Mode
 	,	selected       :: [UnitID]
@@ -26,6 +26,8 @@ data World = World
 	,	current_widget :: Maybe Widget
 	}
 
+data Evolving = Evolving | Devolving | Static deriving (Eq)
+
 empty_world :: World
 empty_world = World
 	{	units          = Map.empty
@@ -33,7 +35,7 @@ empty_world = World
 	,	slice          = 0
 	,	time           = 0
 	,	c_history      = []
-	,	evolving       = False
+	,	evolving       = Static
 	,	last_tick      = 0.0
 	,	mode           = ModeSelect
 	,	selected       = []

SpaceTime/src/Graphics.hs
@@ -138,9 +138,9 @@ widget_at widgets (x,y) = case filter (inside (x, y)) widgets of
 -- Handle control events
 events :: Int -> Event -> World -> IO World
 events s_max event @ (EventKey key state (Modifiers Up Up Up) (x,y)) world @ World {time=t, slice=sl, evolving = e}
-	| key == control_left && state   == Up  = return $ world { slice = mod (sl-1) s_max }
-	| key == control_right && state  == Up  = return $ world { slice = mod (sl+1) s_max }
-	| key == control_evolve && state == Up  = return $ world { evolving = not e }
+	| key == control_left  = if state == Up then return $ world { evolving = Static } else return $ world { evolving = Devolving }
+	| key == control_right = if state == Up then return $ world { evolving = Static } else return $ world { evolving = Evolving  }
+	| key == control_evolve && state == Up = return $ world { evolving = if e == Evolving then Static else Evolving }
 	| key == (MouseButton LeftButton) = mouse world event
 	-- | key == (MouseButton LeftButton) && state == Up   = return $ click x y (world {mouse_l_down = False})
 	-- | key == (MouseButton LeftButton) && state == Down = return $ world {mouse_l_down = True}

SpaceTime/src/Main.hs
@@ -39,10 +39,13 @@ main = do
 
 run :: Float -> World -> IO World
 run f (world @ World {time = t, last_tick = l_tick, evolving = evolve}) = 
-	if evolve 
-		then return $ tick_if_time
-		else return $ advance_main_time
+	if evolve == Static
+		then return $ advance_main_time
+		else return $ tick_if_time evolve
 	where
-	tick_if_time = if (l_tick + 0.1) < t then advance_main_time_and_slice else advance_main_time
+	tick_if_time e = if (l_tick + 0.1) < t then advance_main_time_and_slice e else advance_main_time
+	advance_main_time_and_slice e =
+		if e == Evolving
+			then tick_forward $ (advance_main_time) {last_tick = f+t}
+			else tick_back    $ (advance_main_time) {last_tick = f+t}
 	advance_main_time = main_time_forward f world
-	advance_main_time_and_slice = tick_forward $ (main_time_forward f world) {last_tick = f+t}

commit 484a9b106c39f40f0ddc592ac05a82ff633de655

Fixed crash due to ticking back past 0 that was introduced in 3f2aef3cfcfbff94ba847ac15c79f08fee7b60b2.

SpaceTime/src/World.hs
@@ -52,7 +52,7 @@ tick_forward :: World -> World
 tick_forward world @ World {slice = i} = world {slice = mod (i+1) sliceMax}
 
 tick_back :: World -> World
-tick_back world @ World {slice = i} = world {slice = i-1`mod` sliceMax}
+tick_back world @ World {slice = i} = world {slice = mod (i-1) sliceMax}
 
 main_time_forward :: Float -> World -> World
 main_time_forward f (world @ World {time = t}) = world {time = t+f}